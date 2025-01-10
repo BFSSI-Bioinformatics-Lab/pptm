ddac2c65a620ae1dc8c18c031153550a59e184ca
Kate Cook <kate.cook@gmail.com>
2025-01-10 12:06:48 -0500

    add url prefix

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,17 @@ from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
-urlpatterns = [
+URL_PREFIX = os.getenv('DJANGO_URL_PREFIX', '').strip('/')
+
+def prefix_url_patterns(patterns):
+    if not URL_PREFIX:
+        return patterns
+    
+    return [
+        path(f'{URL_PREFIX}/', include((patterns, 'prefixed'), namespace=None))
+    ]
+
+raw_urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "about/",
@@ -29,7 +39,7 @@ urlpatterns = [
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
-    urlpatterns += [
+    raw_urlpatterns += [
         path(
             "400/",
             default_views.bad_request,
@@ -50,4 +60,6 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        raw_urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + raw_urlpatterns
+
+urlpatterns = prefix_url_patterns(raw_urlpatterns)
