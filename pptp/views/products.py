540698246070db808e4bc7216282672781deec53
Kate Cook <kate.cook@gmail.com>
2025-02-20 11:24:04 -0500

    remove get_common_context()

--- pptp/views/products.py
+++ pptp/views/products.py
@@ -28,7 +28,6 @@ class BaseProductTemplateView(BaseProductView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context.update(self.get_common_context())
         return context
     
 class ProductDashboardView(BaseProductTemplateView, TemplateView):
